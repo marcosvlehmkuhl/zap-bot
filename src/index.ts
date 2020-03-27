2a2111ba558e05c3111a98fd2b9d3f6ce826249d
marcoslehmkuhl <m.v.lehmkuhl@gmail.com>
2020-03-27 11:52:33 -0300

    feat: bot.

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,30 @@
-import http from 'http'
+import express from 'express'
+import twilio from 'twilio'
+import bodyParser from 'body-parser'
 
-const hostname = '0.0.0.0'
-const port = 3000
+const MessagingResponse = twilio.twiml.MessagingResponse
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200
-  res.setHeader('Content-Type', 'text/plain')
-  res.end('Hello World!\n')
+const app = express()
+
+console.log(twilio.twiml)
+
+app.use(bodyParser.urlencoded({ extended: false }))
+// parse application/json
+app.use(bodyParser.json())
+
+app.get('/', (req, res) => {
+  res.send('hello world')
 })
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`)
+app.post('/bot', (req, res) => {
+  console.log(req.body)
+  const twiml = new MessagingResponse()
+  twiml.message('The Robots are coming! Head for the hills!')
+
+  res.writeHead(200, { 'Content-Type': 'text/xml' })
+  res.end(twiml.toString())
+})
+
+app.listen(3000, () => {
+  console.log('listening on port 3000')
 })
